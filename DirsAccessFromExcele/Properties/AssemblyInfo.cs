770e55aaee8f7887882646d63d457b4389268208
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-23 09:06:17 +0300

    Переименовать проект

--- DirsAccessFromExcele/Properties/AssemblyInfo.cs
+++ DirsAccessFromExcele/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DirsAccessFromExcele")]
+[assembly: AssemblyTitle("DirsAccessFromExcel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DirsAccessFromExcele")]
+[assembly: AssemblyProduct("DirsAccessFromExcel")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
